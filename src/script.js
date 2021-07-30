a246c43dfd41aca4980e636466b264efa37e5db3
Juani Kitroser <jpkitro@gmail.com>
2021-07-30 12:16:10 -0300

    Finish scroll

---
+++ src/script.js
@@ -0,0 +1,49 @@
+const requestTarget = document.querySelector('#request-target');
+const itemContainer = document.querySelector('#item-container');
+const intersectionOptions = {
+	threshold: 1,
+};
+
+let apiUrl = 'https://rickandmortyapi.com/api/character';
+let loading = false;
+
+const onIntersect = ([entry]) => {
+	if (apiUrl && !loading && entry.isIntersecting) makeRequest(); // Si el llegamos a la pagina que contiene null, no hace request
+};
+
+const makeRequest = () => {
+	loading = true;
+	fetch(apiUrl)
+		.then((response) => response.json())
+		.then((data) => {
+			cleanUp(data.info.next);
+			renderItems(data.results);
+		});
+};
+
+const cleanUp = (nextPage) => {
+	apiUrl = nextPage;
+	loading = false;
+};
+
+const renderItems = (results) => {
+	results.forEach((item) => {
+		itemContainer.appendChild(createItem(item));
+	});
+};
+
+const createItem = (item) => {
+	const newItem = document.createElement('div');
+	newItem.classList.add('item');
+	newItem.innerHTML = `
+            <div class="char-id">${item.id}</div>
+            <div class="char-name">${item.name}</div>
+            <img class="char-img" src=${item.image} />
+            <div class="char-status">${item.status}</div>
+        `;
+	return newItem;
+};
+
+let observer = new IntersectionObserver(onIntersect, intersectionOptions);
+
+observer.observe(requestTarget);
